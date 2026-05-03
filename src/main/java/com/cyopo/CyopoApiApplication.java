02ba54e3d61dc0c9017a0e8968f55dd23fc36cb5
karthikarji <ak28@iitbbs.ac.in>
2026-05-03 05:52:20 +0530

    Initial Setup

---
+++ src/main/java/com/cyopo/CyopoApiApplication.java
@@ -0,0 +1,13 @@
+package com.cyopo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyopoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyopoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cyopo/CyopoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyopo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyopoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
